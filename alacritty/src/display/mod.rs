5d5f4cbe2e9ea3701a46973257dc385c7014d25a
Jason (Jin-Hwan) Yu <jin-hwan.yu@oracle.com>
2023-08-26 21:39:07 +0900

    Replace broken unicode symbols

--- alacritty/src/display/mod.rs
+++ alacritty/src/display/mod.rs
@@ -1316,7 +1316,7 @@ impl Display {
 
         let percent = ((total_lines - 1) - line) * 100 / (total_lines - 1);
         // let text = format!("[{}/{}]", line, total_lines - 1);
-        let text = format!(" 🞀 {}% [{}/{}] 🞂 ", percent, line, total_lines - 1);
+        let text = format!("❰ {}% [{}/{}] ❱", percent, line, total_lines - 1);
         let column = Column(self.size_info.columns().saturating_sub(text.len()));
         let point = Point::new(0, column);
 
